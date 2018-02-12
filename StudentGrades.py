75153b074e45dd79ac336a66edf23dcf43718fd8
Michael Gentile <gentilm5@miamioh.edu>
2018-02-12 10:32:58 -0500

    Variable Rename
    Refactor to fancy underscores

--- StudentGrades.py
+++ StudentGrades.py
@@ -1,43 +1,43 @@
 #Process: Read all the data into one list.
-#Follow up sorting 
+#Follow up sorting
 
-masterList = []
-resultList = []
+master_list = []
+result_list = []
 output = ""
 file = open('studentGrades.txt')
 while 1:
-    fullPersonInfo = file.readline()
-    if not fullPersonInfo : break
-    masterList.append(fullPersonInfo)
+    full_person_info = file.readline()
+    if not full_person_info : break
+    master_list.append(full_person_info)
 #create the averaging data
 file.close()
-for x in range(len(masterList)):
+for x in range(len(master_list)):
     #first is a string
     #followed by 3 quiz scores, followed by  5 HW and 2 exams
     #weighted as follows
     #20% then 30% then 50%
-    tempList = masterList[x].split()
+    temp_list = master_list[x].split()
     counter = 0
-    gradeAverage = 0.0
-    gradeTemp = 0.0
-    resultList.append(tempList[0])
+    grade_average = 0.0
+    grade_temp = 0.0
+    result_list.append(temp_list[0])
     for y in range(1, 4):
-        gradeTemp+=float(tempList[y])
-    gradeAverage = gradeAverage + ((gradeTemp *.2)/3)
-    gradeTemp = 0.0
+        grade_temp+=float(temp_list[y])
+    grade_average = grade_average + ((grade_temp *.2)/3)
+    grade_temp = 0.0
     for y in range(5, 9):
-        gradeTemp+=float(tempList[y])
-    gradeAverage = gradeAverage + ((gradeTemp *.2)/5)
+        grade_temp+=float(temp_list[y])
+    grade_average = grade_average + ((grade_temp *.2)/5)
     for y in range(9, 11):
-        gradeTemp+=float(tempList[y])
-    gradeAverage = gradeAverage + ((gradeTemp *.2)/2)
-    resultList.append(gradeAverage)
-print resultList
+        grade_temp+=float(temp_list[y])
+    grade_average = grade_average + ((grade_temp *.2)/2)
+    result_list.append(grade_average)
+print result_list
 
 file=open("output.txt", "w")
-for x in range(0,len(resultList),2):
-    file.write('{:>20}'.format(str(resultList[x])))
-    score = float(resultList[x+1])
+for x in range(0,len(result_list),2):
+    file.write('{:>20}'.format(str(result_list[x])))
+    score = float(result_list[x+1])
     file.write('{:>5}'.format("%.1f" % score))
     file.write("\n")
 #create the output
