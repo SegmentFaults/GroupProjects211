eefe5bd4d575b6aaf78252fe61665a7455c82188
mg3030z <gentilm5@miamioh.edu>
2018-02-27 08:47:10 -0500

    Update library_books.py

--- Homework2/library_books.py
+++ Homework2/library_books.py
@@ -2,7 +2,9 @@
 # Dr. Alomari
 # Python 3.6.2
 
-
+#param: the file and the file path that you would like to use. 
+#required: the file must be in the ordered requested. 
+#return: a dictionary filled with all of the books found within the file
 def readfile(filepath):
     inner_books = dict()
     file = open(filepath, "r")
@@ -23,7 +25,9 @@ def readfile(filepath):
     information_list.clear()
     return inner_books
 
-
+#params: the key to the book and the actual value of the book
+#returns: nothing
+#function: to print out the information of the book. 
 def print_book(key, value):
     print(key, "\t", end="")
     first = True
@@ -35,7 +39,10 @@ def print_book(key, value):
             print(", " + i, end="")
     print(". ")
 
-
+#Main
+#This reads in the predefined data and and then requests the user for input.
+#if it is found it will print the book data
+#otherwise, it will tell them it wasn't found and exit. 
 books = readfile("Step1Data.txt")
 user_key = input("Please enter a key")
 try:
