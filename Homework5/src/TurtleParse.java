f91f298c580d5eff0543c9e08edb1e83770eeb57
Hutcharmy <nick@hutchfam.net>
2018-04-25 10:39:46 -0400

    wrote part 3

--- Homework5/src/TurtleParse.java
+++ Homework5/src/TurtleParse.java
@@ -5,16 +5,16 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class TurtleParse {
-	static HashMap<String, Integer> variables = new HashMap();
-	static ArrayList<String> commands = new ArrayList<String>();
-	static ArrayList<String> runStrings = new ArrayList<String>();
+	private static HashMap<String, Integer> variables = new HashMap<String, Integer>();
+	private static ArrayList<String> commands = new ArrayList<>();
+	static ArrayList<String> runStrings = new ArrayList<>();
 	static ArrayList<Integer> values = new ArrayList<Integer>();
-	static DrawableTurtle turtle = new DrawableTurtle();
-	static int index = 0;
+	private static DrawableTurtle turtle = new DrawableTurtle();
+	private static int index = 0;
 	public static void main(String[] args) throws FileNotFoundException {
 //		read("testProgramStep1.txt");
-		read("testProgramStep2.txt");
-//		read("testProgramStep3.txt:");
+//		read("testProgramStep2.txt");
+		read("testProgramStep3.txt");
 		turtle.draw();
 	}
 	static private void read(String filename) throws FileNotFoundException {
@@ -41,7 +41,7 @@ public class TurtleParse {
 		
 	}
 	static private void statementList() {
-		if(commands.get(index).equals("forward") || commands.get(index).equals("turn") || commands.get(index).equals("loop")) {
+		if(!(commands.get(index).equals("begin") || commands.get(index).equals("end"))) {
 			statement();
 			statementList();
 		}
@@ -50,13 +50,29 @@ public class TurtleParse {
 		if(commands.get(index).equals("forward") || commands.get(index).equals("turn")) {
 			command();
 		}
-		else {
+		else if (commands.get(index).equals("loop")){
 			loop();
 		}
+		else {
+			assignment();
+		}
+	}
+
+	static private void assignment() {
+		if (commands.get(index + 1).equals("=") && Integer.parseInt(commands.get(index + 2)) >= 0){
+			variables.put(commands.get(index), Integer.parseInt(commands.get(index + 2)));
+		}
+		index += 3; //skip over variable name, equals sign, and value
 	}
 	static private void loop() {
 		index++;
-		int loopCount = Integer.parseInt(commands.get(index));
+		int loopCount;
+		if (variables.containsKey(commands.get(index))){
+		    loopCount = variables.get(commands.get(index));
+        }
+        else {
+            loopCount = Integer.parseInt(commands.get(index));
+        }
 		index++;
 		int loopBegin = index;
 		for(int i = 0; i<loopCount-1; i++) {
@@ -65,17 +81,25 @@ public class TurtleParse {
 		}
 		block();
 		index++;
-		
-		
 	}
 	static private void command() {
 		if(commands.get(index).equals("forward")) {
 			index++;
-			turtle.forward(Integer.parseInt(commands.get(index)));
+			if (variables.containsKey(commands.get(index))) {
+			    turtle.forward(variables.get(commands.get(index)));
+            }
+            else {
+                turtle.forward(Integer.parseInt(commands.get(index)));
+            }
 		}
 		else {
 			index++;
-			turtle.turn(Integer.parseInt(commands.get(index)));
+            if (variables.containsKey(commands.get(index))) {
+                turtle.turn(variables.get(commands.get(index)));
+            }
+            else {
+                turtle.turn(Integer.parseInt(commands.get(index)));
+            }
 		}
 		index++;
 	}
